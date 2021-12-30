aa65250e7cb837f6603e647104d028d2f652141b
reload1bronze <nature9th@gmail.com>
2021-12-30 14:21:02 +0900

    6장: 데이터 타입

---
+++ src/ch-06__data-type/index.js
@@ -0,0 +1,16 @@
+console.log(1 === 1.0);
+
+console.log(10 / 6);
+
+//test
+console.log("1" + 1);
+console.log(2);
+
+// 템플릿 리터럴 내의 표현식
+console.log(`1 + 1 = ${1 + 2}`);
+
+let foo = true;
+foo = 3;
+console.log(foo);
+
+console.log(typeof foo);
