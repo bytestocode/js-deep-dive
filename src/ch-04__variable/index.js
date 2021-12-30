ccf4f066e737769842853565d9dd9878f5438e95
reload1bronze <nature9th@gmail.com>
2021-12-30 14:20:37 +0900

    4장: 변수

---
+++ src/ch-04__variable/index.js
@@ -0,0 +1,25 @@
+let 변수;
+
+console.log(변수); // undefined
+
+console.log(score); // undefined
+
+var score = 100;
+
+// console.log(letScore); // Error: Cannot access 'letScore' before initialization
+//
+// let letScore = 100;
+
+console.log(score2); // undefined
+
+score2 = 80;
+var score2;
+
+console.log(score2); // 80
+
+// const
+const arr = { a: 1, b: 2 };
+
+arr.a = 10;
+
+console.log(arr);
