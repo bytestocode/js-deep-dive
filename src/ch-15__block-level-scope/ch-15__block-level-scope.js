65fa3c47b08950066562d589b6f0ae6f535e7d16
reload1bronze <nature9th@gmail.com>
2022-01-06 14:23:04 +0900

    15장: let, const 키워드와 블록 레벨 스코프

--- src/ch-15__block-level-scope/ch-15__block-level-scope.js
+++ src/ch-15__block-level-scope/ch-15__block-level-scope.js
@@ -11,17 +11,15 @@ let foo = 1;
 console.log(foo); // 1
 // console.log(bar); // ReferenceError: bar is not defined
 
-
 // 15.2.3 변수 호이스팅
-// 예제 15-09 
-// console.log(foo2); // ReferenceError: bar is not defined
+// 예제 15-09
+console.log(foo2); // ReferenceError: Cannot access 'foo2' before initialization
 
 let foo2;
 console.log(foo2); // undefined
 
 foo2 = 1;
 console.log(foo2); // 1
-
 
 // 예제 15-10
 let foo3 = 1;
@@ -31,13 +29,12 @@ let foo3 = 1;
   let foo3 = 2;
 }
 
-
 // 15.3 const 키워드
 // 예제 15-18
 const TAX_RATE = 0.1;
 
 let preTexPrice = 100;
 
-let afterTaxPrice = preTexPrice + (preTexPrice * TAX_RATE);
+let afterTaxPrice = preTexPrice + preTexPrice * TAX_RATE;
 
 console.log(afterTaxPrice);
